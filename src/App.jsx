978f62ea7c0a99370531a2e9c93c338c5be8d9d3
Pysiiak <Peanut1223@gmail.com>
2023-06-24 12:45:05 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Country from './pages/Country'
+import { useState } from "react"
+
+function App() {
+  const th = localStorage.getItem('CntrsTh')
+  const [theme,setTheme] = useState(th || 'light')
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Navbar setTheme={setTheme} theme={theme}/>}>
+        <Route index element={<Home/>}/>
+        <Route path="/country/:name" element={<Country/>}/>
+      </Route>
+    )
+  )
+
+  return (
+    <div className="app" data-theme={theme}>
+      <RouterProvider router={router}/>
+    </div>
+  )
+}
+
+export default App
